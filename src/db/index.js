5f5ed7f63ff05052b981eb7df720b18f4bd7bcf5
Mubashir <app@apps-MacBook-Pro.local>
2024-10-30 14:37:17 +0500

    feat: db is connect and working fine

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+import { DB_NAME } from "../constants.js";
+
+
+const connectDb = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGO_DB_URL}/${DB_NAME}`)
+        console.log(`\n Mongo DB connection established DB host: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("Mongoose db connection FAILED", error);
+        process.exit(1);
+    }
+}
+
+export default connectDb;
