17a855ce930c82a763c5ce8126b148255a425a2c
Mubashir <app@apps-MacBook-Pro.local>
2024-10-31 11:10:16 +0500

    feat: api errors and responses

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import express from "express";
 import dotenv from "dotenv"
 
 import connectDb from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: "./env"
 })
 
-connectDb();
+connectDb().then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT}`)
+    })
+}).catch((error) => {
+    console.log("Mongo db failed to connect", error);
+});
 
 // const app = express();
 
