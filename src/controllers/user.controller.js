0a56551b7f5338b2b86b1a2b32bd41bea0422d59
Mubashir <app@apps-MacBook-Pro.local>
2024-11-13 17:47:10 +0500

    bug(USER_CONTROLLER): update the path

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,10 +3,10 @@ import mongoose, { Schema } from "mongoose";
 
 
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { ApiError } from "../utils/ApiError.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { User } from "../models/user.model.js"
 import { ApiResponse } from "../utils/apiResponse.js"
+import { ApiError } from "../utils/ApiError.js";
 
 
 //just pass the id if will create the new token and save them into db
