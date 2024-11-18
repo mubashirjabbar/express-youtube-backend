3d292bf24aa1550355b5ca7de29d7741c3156e6a
Mubashir <app@apps-MacBook-Pro.local>
2024-11-18 13:29:56 +0500

    bug(apiError): change the fil path

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,7 +6,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { User } from "../models/user.model.js"
 import { ApiResponse } from "../utils/apiResponse.js"
-import { ApiError } from "../utils/ApiError.js";
+import { ApiError } from "../utils/apiError.js";
 
 
 //just pass the id if will create the new token and save them into db
